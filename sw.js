9653d2ae18863972d364847a723687241da07578
Zemke <florian@zemke.io>
2020-05-02 13:03:44 +0200

    Service Worker all the way

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'offline-fallback';
+
+self.addEventListener('install', e => {
+  console.log("The Service Worker is being installed.");
+  e.waitUntil(async () => {
+    const cache = await self.caches.open(CACHE);
+    await cache.addAll(['/', '/index.html']);
+    return self.skipWaiting();
+  });
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return caches.open(CACHE).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
