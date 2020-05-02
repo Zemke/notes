37239a6dcf84e0bb9f2f00fede88625243624bb0
Zemke <florian@zemke.io>
2020-05-02 20:53:43 +0200

    POST requests cannot be cached

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((resp) => {
       return resp || fetch(event.request).then((response) => {
+        if (event.request.method === 'POST') return response;
         return caches.open(CACHE).then((cache) => {
           cache.put(event.request, response.clone());
           return response;
